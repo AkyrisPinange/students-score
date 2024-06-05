f7f95602dd4e38ae8d800646a78ff42d4f71abb6
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2024-06-05 17:01:20 -0300

    add Services

---
+++ StudantScore/Services/AlunoService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudantScore.Models;
+using StudantScore.Repositories;
+using StudantScore.Strategies;
+
+namespace StudantScore.Services
+{
+    public class AlunoService : IAlunoService
+    {
+        private readonly IAlunoRepository _alunoRepository;
+
+        public AlunoService(IAlunoRepository alunoRepository) 
+        {
+            _alunoRepository = alunoRepository;
+        }
+
+        public IEnumerable<Aluno> GetAlunosAprovados()
+        {
+            return _alunoRepository.GetAll()
+                .Where(a => a.Materias.All(m => m.Nota >= 60));
+        }
+
+        public IEnumerable<Aluno> GetAlunosReprovados()
+        {
+            return _alunoRepository.GetAll()
+                .Where(a => a.Materias.All(a => a.Nota < 60));
+        }
+
+        public Aluno GetMelhorAlunoPorMateria(string materia) 
+        {
+            return _alunoRepository.GetAll()
+                .SelectMany(a => a.Materias.Where(m => string.Equals(m.Nome, materia, 
+                StringComparison.OrdinalIgnoreCase))
+                .Select(m => new { Aluno = a, m.Nota }))
+                .OrderByDescending(a => a.Nota)
+                .FirstOrDefault()?.Aluno;
+        }
+
+        public Aluno GetAlunoPorMatricula(int matricula)
+        {
+            return _alunoRepository.GetAll()
+                .Where(a => a.Matricula == matricula)
+                .FirstOrDefault();
+        }
+
+
+        public IEnumerable<Aluno> OrdenarAlunosPorMedia(ISortingStrategy strategy)
+        {
+            var alunos = _alunoRepository.GetAll();
+            return strategy.Sort(alunos);
+        }
+    }
+}
---
+++ StudantScore/Services/IAlunoService.cs
@@ -0,0 +1,16 @@
+﻿using StudantScore.Models;
+using StudantScore.Strategies;
+
+namespace StudantScore.Services
+{
+    public interface IAlunoService
+    {       
+             
+            IEnumerable<Aluno> GetAlunosAprovados();
+            IEnumerable<Aluno> GetAlunosReprovados();
+            Aluno GetMelhorAlunoPorMateria(string materia);
+            Aluno GetAlunoPorMatricula(int matricula);
+            IEnumerable<Aluno> OrdenarAlunosPorMedia(ISortingStrategy sortingStrategy);
+       
+    }
+}
