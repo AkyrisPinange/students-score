6984e151ee1e54158e156e701683262017840192
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2024-06-05 16:56:07 -0300

    add models

---
+++ StudantScore/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudantScore.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Matricula { get; set; }
+
+        public  string Nome { get; set; }
+       
+        public  ICollection<Materia> Materias { get; set; }
+    }
+}
---
+++ StudantScore/Models/Materia.cs
@@ -0,0 +1,11 @@
+﻿namespace StudantScore.Models
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public int Nota { get; set; }
+        public int AlunoId { get; set; }
+        public required Aluno Aluno { get; set; }
+    }
+}
